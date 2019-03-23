c6072eed8168b2fc40cbf517cee92f52fa662472
Wierzkac <wierzkac@gmail.com>
2019-03-23 23:05:37 +0100

    inserty

--- JediLibraryWebApp/Default.aspx.cs
+++ JediLibraryWebApp/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace JediLibraryWebApp
 {
@@ -11,6 +12,26 @@ namespace JediLibraryWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=192.168.43.215,3306; Network Library = DBMSSOCN; Initial Catalog=JediLibraryDB;User ID=Yoda;Password=Plagiacik12";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                Label2.Text = "Connection Opened!";
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Label2.Text = "Cannot open connection! ";
+            }
+
 
         }
     }
